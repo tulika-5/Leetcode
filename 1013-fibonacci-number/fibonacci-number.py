0f392c2086c2befcbdd8dd3e03bdc2d4b353490a
Tulika <111385523+tulika-5@users.noreply.github.com>
2024-04-14 22:33:26 +0530

    Update fibonacci-number.py

--- 1013-fibonacci-number/fibonacci-number.py
+++ 1013-fibonacci-number/fibonacci-number.py
@@ -1,2 +1,12 @@
 class Solution:
-    def 
+    def fib(self, n: int) -> int:
+        if n == 0:
+            return 0
+        elif n == 1:
+            return 1
+        
+        f1, f2 = 0, 1
+        for _ in range(2, n + 1):
+            f1, f2 = f2, f1 + f2
+        return f2
+
